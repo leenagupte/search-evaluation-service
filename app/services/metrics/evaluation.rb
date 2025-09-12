a6b519cd22e303a0501ef766be93dc2b6288c63a
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2025-09-12 16:54:07 +0100

    Add metric evaluation
    Audit trail:
    https://github.com/alphagov/search-api-v2/blob/601f3c4f8e256df57502390c119d9f602ef06fd4/app/services/metrics/evaluation.rb
    https://github.com/alphagov/search-api-v2/blob/601f3c4f8e256df57502390c119d9f602ef06fd4/spec/services/metrics/evaluation_spec.rb

---
+++ app/services/metrics/evaluation.rb
@@ -0,0 +1,42 @@
+module Metrics
+  class Evaluation
+    TOP_K_LEVELS = %w[1 3 5 10].freeze
+
+    def initialize(registry)
+      @doc_recall = registry.gauge(
+        :search_api_v2_evaluation_monitoring_recall,
+        docstring: "Vertex AI search evaluation recall",
+        labels: %i[top month dataset],
+      )
+      @doc_precision = registry.gauge(
+        :search_api_v2_evaluation_monitoring_precision,
+        docstring: "Vertex AI search evaluation precision",
+        labels: %i[top month dataset],
+      )
+      @doc_ndcg = registry.gauge(
+        :search_api_v2_evaluation_monitoring_ndcg,
+        docstring: "Vertex AI search evaluation ndcg",
+        labels: %i[top month dataset],
+      )
+    end
+
+    def record_evaluations(evaluation_result, month, dataset)
+      metrics.each { |key, registry| record_evaluation(key, registry, month, dataset, evaluation_result) }
+    end
+
+  private
+
+    attr_reader :doc_recall, :doc_precision, :doc_ndcg
+
+    def metrics
+      { doc_recall:, doc_precision:, doc_ndcg: }
+    end
+
+    def record_evaluation(key, registry, month, dataset, evaluation_result)
+      TOP_K_LEVELS.each do |k|
+        value = evaluation_result.dig(key, :"top_#{k}")
+        registry.set(value, labels: { top: k, month:, dataset: }) if value.present?
+      end
+    end
+  end
+end
